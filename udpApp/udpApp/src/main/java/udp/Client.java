f5394fde8caf261541b332cc61d110df897768a6
Ahmad <ahmad.jano@heig-vd.ch>
2023-12-19 07:20:16 +0100

    Refactor Client.java
    1. Extract Common Code into Methods: The code for sending and receiving UDP packets is repeated. This can be refactored into separate methods.
    2. Command Processing: Refactor command processing to reduce complexity.
    3. Constants for Command Strings: Use constants for repeated strings like "LIST".
    4. Buffer Size: Define a constant for buffer size to avoid magic numbers.

--- udpApp/udpApp/src/main/java/udp/Client.java
+++ udpApp/udpApp/src/main/java/udp/Client.java
@@ -3,15 +3,17 @@ package udp;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Client {
-    private DatagramSocket socket;
-    private InetAddress serverAddress;
-    private int serverPort;
-    private static final int TIMEOUT = 5000; // Timeout in milliseconds
-    private static final int MAX_RETRIES = 2; // Maximum number of retries
+    private final DatagramSocket socket;
+    private final InetAddress serverAddress;
+    private final int serverPort;
+    private static final int TIMEOUT = 5000;
+    private static final int MAX_RETRIES = 2;
+    private static final int BUFFER_SIZE = 2048;
+    private static final String COMMAND_LIST = "LIST";
+    private static final String COMMAND_LOG = "LOG";
 
     public Client(String serverIp, int serverPort) throws Exception {
         this.socket = new DatagramSocket();
@@ -20,84 +22,55 @@ public class Client {
         this.socket.setSoTimeout(TIMEOUT);
     }
 
-    public String requestLog(String deviceIp) throws Exception {
-        String request = "LOG " + deviceIp;
+    private String sendRequest(String request) throws Exception {
         byte[] buffer = request.getBytes();
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
         socket.send(packet);
-        buffer = new byte[1024];
+        buffer = new byte[BUFFER_SIZE];
         packet = new DatagramPacket(buffer, buffer.length);
         socket.receive(packet);
         return new String(packet.getData(), 0, packet.getLength());
+    }
+
+    public String requestLog(String deviceIp) throws Exception {
+        return sendRequest(COMMAND_LOG + " " + deviceIp);
     }
 
     public String requestStatus(String deviceIp) throws Exception {
-        byte[] buffer = deviceIp.getBytes();
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
-        socket.send(packet);
-        buffer = new byte[2048];
-        packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-        return new String(packet.getData(), 0, packet.getLength());
+        return sendRequest(deviceIp);
     }
 
     public String requestDeviceCount() throws Exception {
-        byte[] buffer = "LIST".getBytes();
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
-        socket.send(packet);
-        buffer = new byte[1024];
-        packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-        return new String(packet.getData(), 0, packet.getLength());
+        return sendRequest(COMMAND_LIST);
+    }
+
+    private void processCommand(String command) {
+        try {
+            String response;
+            if (COMMAND_LIST.equals(command)) {
+                response = requestDeviceCount();
+                System.out.println("Available devices: " + response);
+            } else if (command.startsWith(COMMAND_LOG)) {
+                response = requestLog(command.split(" ")[1]);
+                System.out.println("Status changes for " + command.split(" ")[1] + ":");
+                for (String status : response.split(",")) {
+                    System.out.println(status);
+                }
+            } else {
+                response = requestStatus(command);
+                System.out.println("Status of " + command + ": " + response);
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
     }
 
     public void startRepl() {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter command (device '<ID>' to request current status of device, 'LIST' to get list of available devices, 'LOG <ID>' to get all status updates since start): ");
+        System.out.println("Enter command ('LIST' for devices, '<ID>' for status, 'LOG <ID>' for log):");
+        while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
-            String[] commandParts = command.split(" ");
-            try {
-                if ("LIST".equals(commandParts[0])) {
-                    int retries = 0;
-                    while (retries < MAX_RETRIES) {
-                        try {
-                            String deviceList = requestDeviceCount();
-                            System.out.println("Available devices: " + deviceList);
-                            break;
-                        } catch (SocketTimeoutException e) {
-                            retries++;
-                            if (retries == MAX_RETRIES) {
-                                System.out.println("Request timed out after " + MAX_RETRIES + " attempts.");
-                            }
-                        }
-                    }
-                }else if("LOG".equals(commandParts[0])){
-                    String statusChange = requestLog(commandParts[1]);
-                    //Print the log of the device, each log entry on a new line
-                    String[] statusChanges = statusChange.split(",");
-                    System.out.println("Status changes for " + commandParts[1] + ":");
-                    for(String status : statusChanges){
-                        System.out.println(status);
-                    }
-                } else {
-                    int retries = 0;
-                    while (retries < MAX_RETRIES) {
-                        try {
-                            String status = requestStatus(command);
-                            System.out.println("Status of " + command + ": " + status);
-                            break;
-                        } catch (SocketTimeoutException e) {
-                            retries++;
-                            if (retries == MAX_RETRIES) {
-                                System.out.println("Request timed out after " + MAX_RETRIES + " attempts.");
-                            }
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            processCommand(command);
         }
     }
 }
